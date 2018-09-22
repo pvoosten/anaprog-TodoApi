cffd31199cb5f82b2d80271e0f8213502dd6fe29
Philip van Oosten <pvoosten@gmail.com>
2018-09-22 23:25:39 +0200

    Gedeeltelijke implementatie tests toegevoegd

--- TodoApi/TodoObjectModel/TodoItem.cs
+++ TodoApi/TodoObjectModel/TodoItem.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace TodoObjectModel
 {
-    class TodoItem
+
+    /// <summary>
+    /// Een taak of todo item
+    /// </summary>
+    public class TodoItem
     {
+        /// <summary>
+        /// Uniek identificatienummer van deze taak
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// De titel van een taak
+        /// </summary>
+        public string Titel { get; set; }
+
+        /// <summary>
+        /// Het tijdstip waarop deze taak werd aangeduid als voltooid
+        /// </summary>
+        public DateTime? TijdstipAfgevinkt { get; set; }
+
+        public bool IsAfgevinkt => TijdstipAfgevinkt != null;
     }
 }
--- TodoApi/TodoTests/TakenVoorPersoonTest.cs
+++ TodoApi/TodoTests/TakenVoorPersoonTest.cs
@@ -1,13 +1,71 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using TodoObjectModel;
 
 namespace TodoTests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        private Persoon _persoon;
+
+        [TestInitialize]
+        public void SetUp()
         {
+            _persoon = new Persoon();
+        } 
+
+        [TestMethod]
+        public void MaakNieuweTaak()
+        {
+            var item = _persoon.VoegItemToe("Doe iets");
+            Assert.AreEqual(item, _persoon.TodoItems.First());
         }
+
+        [TestMethod, ExpectedException(typeof(TodoItemBestaatAlException))]
+        public void KanNietTweeKeerDezelfdeTaakToevoegen()
+        {
+            var titel = "Doe dit";
+            _persoon.VoegItemToe(titel);
+            _persoon.VoegItemToe(titel);
+        }
+
+        [TestMethod]
+        public void AantalTakenNaToevoegen()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _persoon.VoegItemToe($"Taak {i}");
+                Assert.AreEqual(i+1, _persoon.AantalActieveTaken);
+            }
+        }
+
+        [DataTestMethod,
+            DataRow(10, 4),
+            DataRow(15,14),
+            DataRow(5,5),
+            DataRow(7,0)]
+        public void AantalActieveTakenNaAfvinken(int aantalAanmaken, int aantalAfvinken)
+        {
+            var items = new TodoItem[aantalAfvinken];
+            for (int i = 0; i < aantalAanmaken; i++)
+            {
+                var taak = _persoon.VoegItemToe($"taak om af te vinken {i}");
+                if(i < aantalAfvinken)
+                {
+                    items[i] = taak;
+                }
+            }
+            // Vink de eerste 5 items af
+            foreach(var taak in items)
+            {
+                taak.VinkAf();
+            }
+            Assert.AreEqual(aantalAanmaken - aantalAfvinken, _persoon.AantalActieveTaken);
+        }
+
+#warning nog extra tests nodig
     }
 }
